cc25642a14d91c46a22ced71b7706e2593d63407
Sam Lanning <sam@samlanning.com>
2015-08-23 02:02:50 +0100

    added pad area

--- src/js/main.js
+++ src/js/main.js
@@ -23,11 +23,17 @@ $(document).ready(function(){
     var _stage_height = $stage.height();
     var _centre_x = _stage_width / 2 | 0;
     var _centre_y = _stage_height / 2 | 0;
+    var _left_pad = [];
 
     // Draw Hexagons
     (function(){
 
       fill_screen_with_hexagons();
+
+      // Draw left pad
+      _left_pad.forEach(function(props){
+        draw_hexagon(props, "pad");
+      });
 
     })();
 
@@ -44,8 +50,36 @@ $(document).ready(function(){
               _min_r --;
 
             for(var _r = _min_r; compute_offset(_q, _r).y < _stage_height + _half_h; _r++)
-              draw_hexagon(compute_offset(_q, _r));
+              draw_hexagon_as_required(_q, _r);
+
+            if(_q === _min_q + 3 ){
+              add_pad_area(_q, _r);
+            }
+
         }
+    }
+
+    function add_pad_area(q, r){
+      var _pad_q = q;
+      var _pad_r = r - 2;
+      var _switched = false;
+      if(compute_offset(_pad_q, _pad_r).y > _stage_height - HEX_HEIGHT){
+        _pad_q ++;
+        _pad_r --;
+        _switched = true;
+      }
+
+      _left_pad = [
+        compute_offset(_pad_q, _pad_r),
+        compute_offset(_pad_q, _pad_r - 1)
+      ];
+      if(!_switched){
+        _left_pad.push(compute_offset(_pad_q + 1, _pad_r - 1));
+        _left_pad.push(compute_offset(_pad_q + 1, _pad_r - 2));
+      } else {
+        _left_pad.push(compute_offset(_pad_q - 1, _pad_r));
+        _left_pad.push(compute_offset(_pad_q - 1, _pad_r - 1));
+      }
     }
 
     function compute_offset(q, r){
@@ -54,12 +88,27 @@ $(document).ready(function(){
         y: _centre_y + q * _y_step + r * HEX_HEIGHT
       };
     }
+
+    function draw_hexagon_as_required(q, r){
+      var _class = null;
+
+      // Centre hexagons
+      if(
+        q === -2 && (r === 0 || r === 1 || r === 2) ||
+        q === -1 && (r === -1 || r === 0 || r === 1 || r === 2) ||
+        q === 0 && (r === -2 || r === -1 || r === 0 || r === 1 || r === 2) ||
+        q === 1 && (r === -2 || r === -1 || r === 0 || r === 1) ||
+        q === 2 && (r === -2 || r === -1 || r === 0))
+        _class = "centre";
+
+      var $hex = draw_hexagon(compute_offset(q, r), _class);
+    }
+
   }
 
-  function draw_hexagon(props){
+  function draw_hexagon(props, cls){
     var _x = props.x;
     var _y = props.y;
-    console.log(_x, _y);
 
     // X Values
     var _x0 = _x - _half_w;
@@ -75,10 +124,12 @@ $(document).ready(function(){
     var _points = _x3 + ',' + _y1 + ' ' + _x2 + ',' + _y2 + ' ' + _x1 + ',' + _y2 + ' ' + _x0 + ',' + _y1 + ' ' + _x1 + ',' + _y0 + ' ' + _x2 + ',' + _y0;
 
     var $polygon = $(createNSElem('polygon'));
-    $polygon.attr('class', 'hex-polygon');
+    var _class = cls === null ? 'hex-polygon' : ('hex-polygon ' + cls);
+    $polygon.attr('class', _class);
     $polygon.attr('points', _points);
 
     $hex_background.append($polygon);
+    return $polygon;
   }
 
   function createNSElem(tag){
