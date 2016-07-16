87bc9181318fa2ccb292a540c33c87be23396b4f
Sam Lanning <sam@samlanning.com>
2016-07-17 01:21:43 +0200

    Allow loading the music file into the browser to play

--- synesthesia-composer/gulpfile.js
+++ synesthesia-composer/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
+var typings = require("gulp-typings");
 
 var tsProject = ts.createProject('scripts/ts/tsconfig.json');
 
@@ -9,12 +10,16 @@ gulp.task('bower', function() {
   return bower();
 });
 
-gulp.task('ts', function () {
+gulp.task("typings", function(){
+    return gulp.src("./typings.json").pipe(typings());
+});
+
+gulp.task('ts', ['typings'], function () {
     return tsProject.src()
       .pipe(sourcemaps.init())
       .pipe(ts(tsProject))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest('scripts/js'));
+      .pipe(gulp.dest('scripts'));
 });
 
 gulp.task('default', ['bower', 'ts']);
--- synesthesia-composer/scripts/ts/main.ts
+++ synesthesia-composer/scripts/ts/main.ts
@@ -1,3 +1,24 @@
+/// <reference path="../../typings/index.d.ts"/>
+
+// Elements
+function fileInput() {
+  return <HTMLInputElement> $('#file_picker').get(0);
+}
+
+function audio() {
+  return <HTMLAudioElement> $('#audio').get(0);
+}
+
 export function start(): void {
   console.log("start")
+  console.log($);
+
+  $('#file_picker').change(loadAudioFile);
+
+}
+
+function loadAudioFile() {
+  const file = fileInput().files[0];
+  console.debug('file', file);
+  audio().src = URL.createObjectURL(document.getElementsByTagName('input')[0].files[0]);
 }
